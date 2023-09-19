f39c784f579da5e5956d87169c6e2eb13f1fd4fe
Goodlife431 <oduwolejohn431@gmail.com>
2023-09-19 17:51:21 +0100

    dashboard

---
+++ react/src/router.jsx
@@ -0,0 +1,46 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Surveys from "./views/Surveys";
+import GuestLayout from "./Component/GuestLayout";
+import DefaultLayout from "./Component/DefaultLayout";
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/"/>
+            },
+            {
+                path: '/',
+                element: <Dashboard/>
+            },
+            {
+                path: '/surveys',
+                element: <Surveys/>
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <Signup/>
+            },
+        ]
+    }
+])
+
+export default router;
+
